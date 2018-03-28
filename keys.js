167812c6ee140e280b2bbbc8ac55fbf2c4af38df
lschmittling <lanceschmittling@Lances-MBP>
2018-03-28 17:02:41 -0500

    Got twitter to finally work in the terminal

--- keys.js
+++ keys.js
@@ -1,13 +1,14 @@
-console.log('this is loaded');
+// Loggin to make sure this file was loaded correctly
+// console.log("this is loaded");
 
 exports.twitter = {
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,6 +1,83 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
+// Reference exercise 18 from class to cycle through arguments
+// Entire argument stored in array
+var nodeArgs = process.argv;
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// Pull out user input in array's 3rd position (input[2])
+var action = nodeArgs[2];
+
+// Empty variable for holding users input
+var userInput = "";
+
+for (var i = 2; i < nodeArgs.length; i++) {
+  if (i > 2 && i < nodeArgs.length) {
+    userInput = userInput + "+" + nodeArgs[i];
+  } else {
+    userInput += nodeArgs[i];
+  }
+}
+
+// Switch-case from class. It will direct which function to run
+switch (action) {
+  case "my-tweets":
+    // Create this function below
+    myTweets();
+    break;
+
+  case "spotify-this-song":
+    // Create this function below
+    spotifyThis();
+    break;
+
+  case "movie-this":
+    // Create this function below
+    movieThis();
+    break;
+
+  case "do-what-it-says":
+    // Create this function below
+    doThis();
+    break;
+}
+
+// Twitter function for calling 20 tweets
+function myTweets() {
+  
+  // FS require
+  var fs = require("fs");
+
+  // Import keys.js file and store it in a variable
+  var keys = require("./keys.js");
+  // console.log(keys.twitter);
+
+  // This is requiring the Twitter NPM
+  var Twitter = require("twitter");
+
+  // This should go through the "./keys.js" file and grab Twitter access keys
+  var client = new Twitter(keys.twitter);
+
+  // Reference twitter npm documentation for below
+  var params = {
+    screen_name: "@schmittling2",
+    count: 20
+  };
+
+  client.get("statuses/user_timeline", params, function(
+    error,
+    tweets,
+    response
+  ) {
+    if (!error) {
+      for (var i = 0; i < tweets.length; i++) {
+        console.log("--------------------------------------");
+        console.log(tweets[i].text);
+        console.log("This was tweeted on: " + tweets[i].created_at);
+        console.log("--------------------------------------");
+      }
+    } 
+  });
+}
+
+// Not sure what this does yet...
+// var spotify = new Spotify(keys.spotify);
