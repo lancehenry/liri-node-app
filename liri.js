95d3ee31cb4f58c4016ca79432fe81216b0a4a7e
lschmittling <lanceschmittling@Lances-MBP>
2018-03-28 10:51:12 -0500

    Added npm per homework instructions

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-var stuffINeed = require("./keys.js");
+var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
