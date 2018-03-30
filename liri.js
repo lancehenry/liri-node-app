a8f40148756f74f8d02ee6bbb6d884fbb19450da
lschmittling <lanceschmittling@Lances-MBP>
2018-03-30 14:41:32 -0500

    Finished up movie-this callback

--- liri.js
+++ liri.js
@@ -20,6 +20,7 @@ for (var i = 3; i < nodeArgs.length; i++) {
 
 // Referenced (15-BankJS) switch-case from class.
 switch (action) {
+
   case "my-tweets":
     // Created this function below
     myTweets();
@@ -72,150 +73,150 @@ function myTweets() {
         console.log("This was tweeted on: " + tweets[i].created_at);
         console.log("--------------------------------------");
         console.log("");
-      } 
+      }
     } else {
       console.log("Something went wrong: " + error);
     }
   });
 }
 
-
 // Spotify function for returning info about a song title
 function spotifyThis() {
-
+  // This if/else statement checks to see if the secondInput is defined or undefined
   if (secondInput != false) {
-    
+    // Import keys.js file and store it in a variable
     var keys = require("./keys.js");
+
+    // This is requiring the Spotify NPM
     var Spotify = require("node-spotify-api");
+
+    // This should go through the "./keys.js" file and grab Spotify access keys
     var spotify = new Spotify(keys.spotify);
 
-    spotify.search({
-      type: "track",
-      query: secondInput
-    },
-    function(err, data) {
-      if (!err) {
+    // Referenced https://www.npmjs.com/package/node-spotify-api
+    spotify.search(
+      {
+        type: "track",
+        query: secondInput
+      },
+      function(err, data) {
+        if (!err) {
+          console.log("");
+          console.log("");
+          console.log("--------------------------------------");
+          console.log("Your song: " + data.tracks.items[0].name);
+          console.log("--------------------------------------");
+          console.log("The artist is: " + data.tracks.items[0].artists[0].name);
+          console.log("From the album: " + data.tracks.items[0].album.name);
+          console.log("You can preview it here: " + data.tracks.items[0].preview_url);
+          console.log("--------------------------------------");
+          console.log("");
+          console.log("");
+        } else {
+          console.log("Something went wrong: " + err);
+        }
+      }
+    );
+  } else {
+    // Import keys.js file and store it in a variable
+    var keys = require("./keys.js");
+
+    // This is requiring the Spotify NPM
+    var Spotify = require("node-spotify-api");
+
+    // This should go through the "./keys.js" file and grab Spotify access keys
+    var spotify = new Spotify(keys.spotify);
+
+    // Referenced https://www.npmjs.com/package/node-spotify-api
+    spotify.search(
+      {
+        type: "track",
+        query: "The+Sign+Ace+of+Base"
+      },
+      function(err, data) {
+        if (!err) {
+          console.log("");
+          console.log("");
+          console.log("--------------------------------------");
+          console.log("Your song: " + data.tracks.items[0].name);
+          console.log("--------------------------------------");
+          console.log("The artist is: " + data.tracks.items[0].artists[0].name);
+          console.log("From the album: " + data.tracks.items[0].album.name);
+          console.log("You can preview it here: " + data.tracks.items[0].preview_url);
+          console.log("--------------------------------------");
+          console.log("");
+          console.log("");
+        } else {
+          console.log("Something went wrong: " + err);
+        }
+      }
+    );
+  }
+}
+
+// Movie function that returns info for the movie entered
+function movieThis() {
+  // This if/else statement checks to see if the secondInput is defined or undefined
+  if (secondInput != false) {
+    // Referenced exercise (18-OMDB_Request_Students) from class for this OMDB call
+    // Request npm package
+    var request = require("request");
+
+    var queryURL ="http://www.omdbapi.com/?t=" + secondInput + "&y=&plot=short&apikey=trilogy";
+    console.log(queryURL);
+
+    request(queryURL, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
         console.log("");
         console.log("");
         console.log("--------------------------------------");
-        console.log("Your song: " + data.tracks.items[0].name);
+        console.log("Your movie: " + JSON.parse(body).Title);
         console.log("--------------------------------------");
-        console.log("The artist is: " + data.tracks.items[0].artists[0].name);
-        console.log("From the album: " + data.tracks.items[0].album.name);
-        console.log("You can preview it here: " + data.tracks.items[0].preview_url);
+        console.log("Released: " + JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Country Produced: " + JSON.parse(body).Country);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
         console.log("--------------------------------------");
         console.log("");
         console.log("");
       } else {
-        console.log("Something went wrong: " + err);
+        console.log("Something went wrong: " + error);
       }
     });
   } else {
-    var keys = require("./keys.js");
-    var Spotify = require("node-spotify-api");
-    var spotify = new Spotify(keys.spotify);
+    // Referenced exercise (18-OMDB_Request_Students) from class for this OMDB call
+    // Request npm package
+    var request = require("request");
 
-    spotify.search({
-      type: "track",
-      query: "The+Sign+Ace+of+Base"
-    },
-    function(err, data) {
-      if (!err) {
+    var queryURL = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy";
+    console.log(queryURL);
+
+    request(queryURL, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
         console.log("");
         console.log("");
         console.log("--------------------------------------");
-        console.log("Your song: " + data.tracks.items[0].name);
+        console.log("Your movie: " + JSON.parse(body).Title);
         console.log("--------------------------------------");
-        console.log("The artist is: " + data.tracks.items[0].artists[0].name);
-        console.log("From the album: " + data.tracks.items[0].album.name);
-        console.log("You can preview it here: " + data.tracks.items[0].preview_url);
+        console.log("Released: " + JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Country Produced: " + JSON.parse(body).Country);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
         console.log("--------------------------------------");
         console.log("");
         console.log("");
       } else {
-        console.log("Something went wrong: " + err);
+        console.log("Something went wrong: " + error);
       }
     });
   }
 }
-
-   
-
-
-// // Spotify function for returning info about a song title
-// function spotifyThis() {
-
-//   // Import keys.js file and store it in a variable
-//   var keys = require("./keys.js");
-
-//   // This is requiring the Spotify NPM
-//   var Spotify = require("node-spotify-api");
-
-//   // This should go through the "./keys.js" file and grab Spotify access keys
-//   var spotify = new Spotify(keys.spotify);
-
-//   // Referenced https://www.npmjs.com/package/node-spotify-api
-//   spotify.search(
-//     {
-//       type: "track",
-//       query: secondInput
-//     },
-//     function(err, data) {
-//       if (!err) {
-//         console.log("");
-//         console.log("");
-//         console.log("--------------------------------------");
-//         console.log("Your song: " + data.tracks.items[0].name);
-//         console.log("--------------------------------------");
-//         console.log("The artist is: " + data.tracks.items[0].artists[0].name);
-//         console.log("From the album: " + data.tracks.items[0].album.name);
-//         console.log("You can preview it here: " + data.tracks.items[0].preview_url);
-//         console.log("--------------------------------------");
-//         console.log("");
-//         console.log("");
-//       } else {
-//         console.log("Something went wrong: " + err);
-//       }
-//     });
-// }
-
-
-
-
-// Need to figure out how to return "Ace of Base: The Sign" if nothing is entered for the song title
-
-// Movie function that returns info for the movie entered
-function movieThis() {
-  
-  // Referenced exercise (18-OMDB_Request_Students) from class for this OMDB call
-  // Request npm package
-  var request = require("request");
-
-  var queryURL = "http://www.omdbapi.com/?t=" + secondInput + "&y=&plot=short&apikey=trilogy";
-  console.log(queryURL);
-
-  request(queryURL, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      console.log("");
-      console.log("");
-      console.log("--------------------------------------");
-      console.log("Your movie: " + JSON.parse(body).Title);
-      console.log("--------------------------------------");
-      console.log("Released: " + JSON.parse(body).Year);
-      console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
-      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-      console.log("Country Produced: " + JSON.parse(body).Country);
-      console.log("Language: " + JSON.parse(body).Language);
-      console.log("Plot: " + JSON.parse(body).Plot);
-      console.log("Actors: " + JSON.parse(body).Actors);
-      console.log("--------------------------------------");
-      console.log("");
-      console.log("");
-    } else {
-      console.log("Something went wrong: " + err);
-    }
-  })
-} 
 
 // Need to figure out how to return "Mr. Nobody" if nothing is entered for the movie title
 
@@ -230,10 +231,9 @@ function doThis() {
     if (error) {
       return console.log(error);
     }
-    
     // Split the contents of data at the ","
     var dataArr = data.split(",");
-    
+
     // Set the secondInput variable as the second index in the returned array
     secondInput = dataArr[1];
 
